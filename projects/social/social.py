48361cdbf44edee7eebd8134669330bcec74433b
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-10-20 18:36:52 -0400

    day 3 mvp

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,11 @@
+import random
+import collections
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -32,10 +37,8 @@ class SocialGraph:
         """
         Takes a number of users and an average number of friendships
         as arguments
-
         Creates that number of users and a randomly distributed friendships
         between those users.
-
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
@@ -45,20 +48,50 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i}")
 
         # Create friendships
+        possible_friendships = []
+
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        random.shuffle(possible_friendships)
+
+        for i in range(num_users * avg_friendships // 2):
+            friendships = possible_friendships[i]
+            self.add_friendship(friendships[0], friendships[1])
 
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
-
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
-
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        queue = collections.deque([])
+        queue.append([user_id])
+
+        # SetDefault so we can get the value of the specified items with the specified keys
+        visited.setdefault(user_id, [user_id])
+        while queue:
+            # Popleft so we can delete an argument from the left end of the dequeue
+            connections = queue.popleft()
+            person = connections[-1]
+            for friend in self.friendships[person]:
+                # If the friend has not been visited
+                if friend not in visited:
+                    # We'll makke a new list of connections
+                    shortest = list(connections)
+                    # Append the friend
+                    shortest.append(friend)
+                    visited[friend] = shortest
+                    queue.append(shortest)
+
         return visited
 
 
